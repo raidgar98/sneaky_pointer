cd3f7ed124e9e2d538b4a688217b0d63ab2b2f72
raidgar98 <raidgar98@onet.pl>
2020-06-05 02:17:39 +0200

    wip

--- sneaky_pointer.hpp
+++ sneaky_pointer.hpp
@@ -5,105 +5,127 @@
 #include <memory>
 #include <cmath>
 #include <limits>
+#include <map>
 
-template<typename T, uint8_t empty_bits = 4>
+template <typename T, uint8_t empty_bits = 4, bool destroy = true>
 class sneaky_pointer
 {
-
-    T * __pointer = nullptr;
-
+    T *__pointer = nullptr;
 public:
-    
-    sneaky_pointer() { set_pointer(nullptr); }
-    sneaky_pointer(const sneaky_pointer& src) { set_pointer( src.get_pointer() ); }
-    sneaky_pointer(sneaky_pointer&& src) { set_pointer(src.__pointer); src.__pointer = nullptr; }
 
-    explicit sneaky_pointer(T * src) { set_pointer(src); }
+    sneaky_pointer() { set_pointer(nullptr); }
+    template <bool val>
+    sneaky_pointer(const sneaky_pointer<T, empty_bits, val> &src)
+    {
+        set_pointer(nullptr);
+        set_pointer(src.get_pointer());
+    }
+    template <bool val>
+    sneaky_pointer(sneaky_pointer<T, empty_bits, val> &&src)
+    {
+        set_pointer(nullptr);
+        __pointer = src.__pointer;
+    }
+
+    explicit sneaky_pointer(T *src)
+    {
+        set_pointer(nullptr);
+        set_pointer(src);
+    }
 
     ~sneaky_pointer()
     {
-        if(__pointer != nullptr)
-        {
-            __pointer = get_pointer();
-            delete __pointer;
-            __pointer = nullptr;
-        }
+        set_pointer(nullptr);
     }
 
     bool get_flag(const unsigned int num) const
     {
-        if(num == 0 || num >= (empty_bits + 1) || __pointer == nullptr)
-            try{ throw std::invalid_argument("has to be in range <1; " +
-                std::to_string((empty_bits)) + ">"); }catch(...){ throw; }
+        if (num == 0 || num >= (empty_bits + 1) || __pointer == nullptr)
+            try
+            {
+                throw std::invalid_argument("has to be in range <1; " +
+                                            std::to_string((empty_bits)) + ">");
+            }
+            catch (...)
+            {
+                throw;
+            }
         int64_t mask = 1;
-        mask <<= (sizeof(T*) * 8) - num;
-        return reinterpret_cast<int64_t>(__pointer) & mask;
+        mask <<= (sizeof(T *) * 8) - num;
+        return !(reinterpret_cast<int64_t>(__pointer) & mask);
     }
 
     void set_flag(const unsigned int num, const bool flag)
     {
-        if(num == 0 || num >= (empty_bits + 1) || __pointer == nullptr)
-            try{ throw std::invalid_argument("has to be in range <1; " +
-                std::to_string((empty_bits)) + ">"); }catch(...){ throw; }
+        if (num == 0 || num >= (empty_bits + 1) || __pointer == nullptr)
+            try
+            {
+                throw std::invalid_argument("has to be in range <1; " +
+                                            std::to_string((empty_bits)) + ">");
+            }
+            catch (...)
+            {
+                throw;
+            }
         int64_t mask = 1;
-        mask <<= (sizeof(T*) * 8) - num;
-        __pointer = reinterpret_cast<T*>(reinterpret_cast<int64_t>(__pointer) ^ mask);
+        mask <<= (sizeof(T *) * 8) - num;
+        __pointer = reinterpret_cast<T *>(reinterpret_cast<int64_t>(__pointer) ^ mask);
     }
-    
-    T * get_pointer() const noexcept
+
+    T *get_pointer() const noexcept
     {
         int64_t temp = reinterpret_cast<int64_t>(__pointer);
         temp <<= (empty_bits + 1);
         temp >>= (empty_bits + 1);
-        return reinterpret_cast<T*>(temp);
+        return reinterpret_cast<T *>(temp);
     }
 
-    void set_pointer(T * ptr) noexcept
+    void set_pointer(T *ptr) noexcept
     {
-        static_assert(sizeof(T*) == 8, "x64 architecture required.");
+        static_assert(sizeof(T *) == 8, "x64 architecture required.");
         static_assert(empty_bits < 31, "too many empty bits.");
         int64_t mask = reinterpret_cast<int64_t>(__pointer);
-        mask >>= (sizeof(T*) * 8) - (empty_bits + 1);
-        mask <<= (sizeof(T*) * 8) - (empty_bits + 1);
-        this->~sneaky_pointer();
-        __pointer = reinterpret_cast<T*>(reinterpret_cast<int64_t>(ptr) ^ mask);
+        mask >>= (sizeof(T *) * 8) - (empty_bits + 1);
+        mask <<= (sizeof(T *) * 8) - (empty_bits + 1);
+        __pointer = reinterpret_cast<T *>(reinterpret_cast<int64_t>(ptr) ^ mask);
     }
 
-    template<typename num_type = uint64_t>
+    template <typename num_type = uint64_t>
     num_type get_number() const noexcept
     {
         int64_t mask = reinterpret_cast<int64_t>(__pointer);
-        mask >>= (sizeof(T*) * 8) - (empty_bits + 1);
+        mask >>= (sizeof(T *) * 8) - (empty_bits + 1);
         return static_cast<num_type>(mask);
     }
 
-    template<typename num_type = uint64_t>
+    template <typename num_type = uint64_t>
     void set_number(num_type num) noexcept
     {
         int64_t ptr = reinterpret_cast<uint64_t>(get_pointer());
         uint64_t mask = static_cast<uint64_t>(num);
-        mask <<= (sizeof(T*) * 8) - (empty_bits + 1);
-        __pointer = reinterpret_cast<T*>( ptr ^ mask );
+        mask <<= (sizeof(T *) * 8) - (empty_bits + 1);
+        __pointer = reinterpret_cast<T *>(ptr ^ mask);
     }
 
-    T * const operator->() const noexcept
+    T *const operator->() const noexcept
     {
-        return static_cast<T * const>(get_pointer());
+        return static_cast<T *const>(get_pointer());
     }
 
-    T& operator*()
+    T &operator*()
     {
         return *get_pointer();
     }
 
-    T& operator[](const size_t idx)
+    T &operator[](const size_t idx)
     {
         return get_pointer()[idx];
     }
+
 };
 
-template<typename T, uint8_t empty_bits>
-std::ostream& operator<<(std::ostream& output, const sneaky_pointer<T, empty_bits>& src)
+template <typename T, uint8_t empty_bits, bool val>
+std::ostream &operator<<(std::ostream &output, const sneaky_pointer<T, empty_bits, val> &src)
 {
     output << (src.get_pointer() == nullptr ? NULL : *src.get_pointer());
     return output;
